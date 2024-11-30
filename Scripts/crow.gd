6f9e447d21f1350c25499eecf79ab711c05d7b6b
Robin Coda Flores Reyes <robin.flores@duocuc.cl>
2024-11-29 23:08:14 -0300

    Walkies s/Sensibilidad de rotación

--- Scripts/crow.gd
+++ Scripts/crow.gd
@@ -6,9 +6,13 @@ const JUMP_VELOCITY = 4.5
 @export var _walking_speed : float = 1
 @export var _acceleration : float = 2
 @export var _deceleration : float  = 4
+@export var _rotation_speed : float = PI * 2
+var _angle_difference : float
 var _xz_velocity : Vector3
 
 @onready var _animation : AnimationTree = $AnimationTree
+@onready var _rig : Node3D = $rig
+
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var _direction : Vector3
@@ -17,6 +21,11 @@ func move(direction : Vector3):
 	_direction = direction
 
 func _physics_process(delta: float) -> void:
+	
+
+	if _direction:
+		_angle_difference = wrapf(atan2(_direction.x, _direction.z) - _rig.rotation.y, -PI, PI)
+		_rig.rotation.y += clamp(_rotation_speed * delta, 0, abs(_angle_difference)) * sign(_angle_difference)
 	
 	_xz_velocity = Vector3(velocity.x, 0, velocity.z)
 	
