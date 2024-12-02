89ee2e1581501e9d877942c3f8f74d3fb3a11c5c
Robin Coda Flores Reyes <robin.flores@duocuc.cl>
2024-12-02 11:56:25 -0300

    2 sounds - 2 prox

--- Scripts/magnet.gd
+++ Scripts/magnet.gd
@@ -1,23 +1,39 @@
 extends Node
 
-@export_category("Variables")
-@export var rango_maximo : float = 10.0  # Rango máximo en unidades.
-@onready var crow_v2 = get_node("/root/Forest/crow_v2")  # Obtener referencia a crow_v2.
-@onready var owl_v2 = get_node("/root/Forest/owl_v2")  # Obtener referencia a owl_v2.
-@onready var sonido_alerta = crow_v2.get_node("AudioStreamPlayer3D")  # Nodo de sonido en crow_v2.
+@export var rango_maximo: float = 10.0  # Rango máximo en metros.
+@export var rango_minimo: float = 2.0   # Rango mínimo en metros.
 
-# Función que verifica la distancia.
-func _process(delta):
+@onready var crow_v2 = get_node("../crow_v2")  # Ruta relativa para encontrar a crow_v2.
+@onready var owl_v2 = get_node("../owl_v2")  # Ruta relativa para encontrar a owl_v2.
+@onready var sonido_rango_max = crow_v2.get_node("Worried_AudioStreamPlayer3D")  # Sonido para rango máximo.
+@onready var sonido_rango_min = crow_v2.get_node("Angry_AudioStreamPlayer3D")  # Sonido para rango mínimo.
+
+# Función que verifica la distancia y maneja los sonidos.
+func _process(_delta):
 	var distancia = crow_v2.global_position.distance_to(owl_v2.global_position)
 
 	if distancia > rango_maximo:
-		if !sonido_alerta.playing:  # Si el sonido no está sonando, lo reproduce.
-			sonido_alerta.play()
-		# Mandamos a owl_v2 de vuelta al rango.
-		mover_owl_v2_a_rango()
+		manejar_sonido(sonido_rango_max, sonido_rango_min)  # Activar sonido máximo.
+		mover_owl_v2_a_rango(rango_maximo)
+	elif distancia < rango_minimo:
+		manejar_sonido(sonido_rango_min, sonido_rango_max)  # Activar sonido mínimo.
+		mover_owl_v2_a_rango(rango_minimo)
+	else:
+		detener_sonidos()  # Detener ambos sonidos si está dentro del rango permitido.
 
-# Función que hace que owl_v2 vuelva al rango.
-func mover_owl_v2_a_rango():
-	var direccion = (crow_v2.global_position - owl_v2.global_position).normalized()  # Dirección hacia crow_v2.
-	var nuevo_pos = crow_v2.global_position - direccion * rango_maximo
+# Función que ajusta la posición de owl_v2 dentro de los rangos.
+func mover_owl_v2_a_rango(rango_objetivo):
+	var direccion = (crow_v2.global_position - owl_v2.global_position).normalized()
+	var nuevo_pos = crow_v2.global_position - direccion * rango_objetivo
 	owl_v2.global_position = nuevo_pos
+
+# Función para manejar la reproducción de sonidos.
+func manejar_sonido(sonido_a_reproducir, sonido_a_detener):
+	if !sonido_a_reproducir.playing:
+		sonido_a_reproducir.play()
+	sonido_a_detener.stop()
+
+# Función para detener todos los sonidos.
+func detener_sonidos():
+	sonido_rango_max.stop()
+	sonido_rango_min.stop()
